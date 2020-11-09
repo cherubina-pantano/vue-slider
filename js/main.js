3dd85b781d1b5dcec14f2151f200a9452dec7412
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-09 16:28:11 +0100

    Arrow Nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('slider vue');
+const root = new Vue({
+  el: '#root',
+  data: {
+      indexPic: 0,
+      pictures: [
+          './images/image1.jpg',
+          './images/image2.jpg',
+          './images/image3.jpg',
+          './images/image4.jpg',
+      ]
+  },
+
+  methods: {
+    nextPic() {
+        this.indexPic += 1;
+
+        if (this.indexPic > (this.pictures.length - 1)) {
+          this.indexPic = 0;
+        }
+    },
+
+    prevPic() {
+      this.indexPic -= 1;
+
+      if (this.indexPic < 0) {
+        this.indexPic = this.pictures.length - 1;
+      }
+    }
+
+  }
+
+});
